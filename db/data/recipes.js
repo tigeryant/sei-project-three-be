ba6c9621dffcc750056afaf2cde53981e36a1435
Deniss Bulai <deniss.bulai@hotmail.com>
2022-01-05 19:33:07 +0200

    snacksssAll

--- db/data/recipes.js
+++ db/data/recipes.js
@@ -3,19 +3,11 @@ export default [
     course: 'Breakfast',
     title: 'Omelette with Turkey and Spinach',
     image: 'https://cdn.app.eative.com/5fb516b34bf1f709598740.jpg',
-<<<<<<< HEAD
-    prepTime: '12 mins',
-    calories: '333Kcal',
-    protein: '23g',
-    carbs: '5g',
-    fats: '24g',
-=======
     prepTime: 12,
     calories: 333,
     protein: 23,
     carbs: 5,
     fats: 24,
->>>>>>> development
     ingredients: [
       '2 small egg/s',
       '2 teaspoons butter',
@@ -89,7 +81,7 @@ export default [
     ],
   },
   {
-    course: 'Snack',
+    course: 'Snacks',
     title: 'Low carb cinnamon roll mug cake',
     image: 'https://cdn.app.eative.com/5fa3da1a62d80134465079.jpg',
     prepTime: 5,
@@ -113,7 +105,7 @@ export default [
     ],
   },
   {
-    course: 'Snack',
+    course: 'Snacks',
     title: 'Tomato and cucumber bites with avocado',
     image: 'https://cdn.app.eative.com/5f0f07dd55d83101275236.jpeg',
     prepTime: 7,
@@ -134,7 +126,7 @@ export default [
     ],
   },
   {
-    course: 'Snack',
+    course: 'Snacks',
     title: 'Crunchy cheese “crackers”',
     image: 'https://cdn.app.eative.com/5ed51886dba8f545451663.jpg',
     prepTime: 7,
@@ -150,6 +142,160 @@ export default [
       'Put small groups of cheese on it (in pancake shape) and bake until they have turned a golden color',
       'Do not overcook, otherwise crackers will be bitter',
       'Allow to cool and enjoy!'
+    ],
+  },
+  {
+    course: 'Snacks',
+    title: 'Title: Peanut butter fat bomb',
+    image: 'https://cdn.app.eative.com/5ebd13251df76445320403.jpg',
+    prepTime: 30,
+    calories: 205,
+    protein: 6,
+    carbs: 8,
+    fats: 18,
+    ingredients: [
+      '1/2 tablespoon coconut oil',
+      '1 drop vanilla extract',
+      '2+1/2 tablespoons peanut butter',
+      '1/2 tablespoon unsweetened cocoa powder'
+    ],
+    preparation: [
+      'Melt coconut oil until smooth',
+      'Add peanut butter and cocoa powder; Whisk until smooth',
+      'Optionally, add a low carb sweetener to taste (or vanilla extract); Whisk until well mixed',
+      'Scoop & drop the fat bomb mixture into cupcake liners',
+      'Freeze for 15-20 minutes, and enjoy'
+    ],
+  },
+  {
+    course: 'Snacks',
+    title: 'Cheesy zucchini chips',
+    image: 'https://cdn.app.eative.com/5f0eb9d9331fd733484703.jpeg',
+    prepTime: 15,
+    calories: 174,
+    protein: 14,
+    carbs: 10,
+    fats: 8,
+    ingredients: [
+      '4 tablespoons shredded parmesan cheese',
+      '1 medium zucchini/s'
+    ],
+    preparation: [
+      'Preheat oven to 425ºF / 220ºC',
+      'Slice the zucchini and place them on a baking sheet with parchment paper',
+      'Top the zucchini slices with parmesan cheese',
+      'Bake for 10-15 minutes, checking them frequently.'
+    ],
+  },
+  {
+    course: 'Snacks',
+    title: 'Avocado deviled eggs',
+    image: 'https://cdn.app.eative.com/5ece30d4a69fb435129743.jpeg',
+    prepTime: 20,
+    calories: 195,
+    protein: 7,
+    carbs: 3,
+    fats: 17,
+    ingredients: [
+      '4 egg/s',
+      '1/2 small avocado/s',
+      '1 lime/s',
+      '1 tablespoon mayonnaisse'
+    ],
+    preparation: [
+      'Fill a small pot with water up to three quarters and bring it to a boil',
+      'Gently add the eggs, and boil for 10 minutes',
+      'Once they are done, remove from the pot and add them to a bowl of cold water; Once cooled down, peel the shell off',
+      'Cut the eggs in half and separate the yolk from the whites',
+      'Combine all of the yolks in a food processor along with the avocado, mayonnaise, 1 tbsp lime juice, salt and pepper to taste; Process until smooth',
+      'Fill the egg white halves with the creamy mixture',
+      'Store the deviled eggs in an air tight container in the fridge to avoid browning.'
+    ],
+  },
+  {
+    course: 'Snacks',
+    title: 'Keto tortilla chips',
+    image: 'https://cdn.app.eative.com/5ec6accad7abc810006847.jpg',
+    prepTime: 30,
+    calories: 200,
+    protein: 11,
+    carbs: 4,
+    fats: 16,
+    ingredients: [
+      '1/2 cup almond flour',
+      '100g / 3.5oz mozzarella cheese',
+      '1 teaspoon garlic powder',
+      'chili powder',
+      '1 small bowl guacamole'
+    ],
+    preparation: [
+      'Preheat oven to 350°F / 175ºC; Line two large baking sheets with parchment paper',
+      'In a microwave safe bowl, melt grated mozzarella - about 1 minute 30 seconds',
+      'Add almond flour, salt, garlic powder, chili powder, and pepper to taste; Using your hands, knead dough a few times until a smooth ball forms',
+      'Place dough between two sheets of parchment paper and roll out into a rectangle ⅛” thick; Using a knife or a pizza cutter, cut dough into triangles',
+      'Spread chips out on prepared baking sheets, and bake until edges are golden and starting to crisp, 12 to 14 minutes',
+      'Let them cool',
+      'Dip into guacamole, and enjoy!'
+    ],
+  },
+  {
+    course: 'Snacks',
+    title: 'Turkey & cheese roll ups',
+    image: 'https://cdn.app.eative.com/5ebe7c20f3528610549441.jpeg',
+    prepTime: 5,
+    calories: 257,
+    protein: 27,
+    carbs: 2,
+    fats: 15,
+    ingredients: [
+      '3 slices deli turkey meat or turkey ham',
+      '3 slices edam cheese'   
+    ],
+    preparation: [
+      'Roll the slices of turkey and cheese together and form 2 tools',
+      'Secure them with a toothpick, and enjoy!'
+    ],
+  },
+  {
+    course: 'Snacks',
+    title: 'Crackers',
+    image: 'https://cdn.app.eative.com/i/120025.jpg',
+    prepTime: 15,
+    calories: 206,
+    protein: 9,
+    carbs: 7,
+    fats: 18,
+    ingredients: [
+      '1/4 cup almond flour',
+      '1 tablespoon ground flaxseed'
+    ],
+    preparation: [
+      'Preheat oven to 350ºF / 175 ºC',
+      'In a bowl, combine the almond flour, ground flaxseed, a pinch of salt and 1 tablespoon of water',
+      'Place the dough on parchment paper and cover with a second piece of parchment paper',
+      'Use a hand roller or your hands to roll out the dough; then cut it into 6 squares and bake in the oven for 10-15 minutes or until the crackers turn golden brown.'
+    ],
+  },
+  {
+    course: 'Snacks',
+    title: 'Keto smoothie',
+    image: 'https://cdn.app.eative.com/5ecf8a6b53a57226599098.jpeg',
+    prepTime: 5,
+    calories: 207,
+    protein: 5,
+    carbs: 22,
+    fats: 12,
+    ingredients: [
+      '1 cup coconut milk',
+      '2 tablespoons heavy cream',
+      '1 cup fresh spinach',
+      '4 small frozen strawberries',
+      '10 frozen raspberries',
+      '10 frozen blackberries'
+    ],
+    preparation: [
+      'In a blender, combine all ingredients and blend until smooth',
+      'Serve and enjoy!'
     ],
   }
 ]
