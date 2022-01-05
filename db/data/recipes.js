0ef1a4987791ffeeac62dfdb99808e80e1e189a2
Aromi Afolabi <aromiafolabi@googlemail.com>
2022-01-05 14:43:44 +0000

    recipe format

---
+++ db/data/recipes.js
@@ -0,0 +1,31 @@
+export default [
+  {
+    course: 'lunch',
+    title: 'Keto taco cups',
+    image: 'https://cdn.app.eative.com/5ee7888028163798738214.jpeg',
+    prepTime: 30,
+    calories: 406,   
+    protein: 30,
+    carbs: 2,
+    fats: 30,
+    ingredients: [
+      '1 tablespoon virgin olive oil',
+      '1/2 small tomatoes',
+      '6 tablespoons cheddar cheese',
+      '3.8oz / 110g ground beef',
+      'paprika powder',
+      'chili powder',
+      'ground cumin',
+      '1 garlic clove/s',
+      '1 tablespoon sour cream'
+    ],
+    preparation: [
+      'Preheat oven to 375°F / 190ºC and line a large baking sheet with parchment paper',
+      'Spoon about 2 tablespoons of grated cheddar per cup a few inches apart (3 taco cups in total); Bake until bubbly and edges are beginning to turn golden, about 6 minutes; Let cool on baking sheet for a minute',
+      'Meanwhile, grease bottom of a muffin tin with cooking spray, then carefully pick up melted cheese slices and place on bottom of muffin tin; Fit with another inverted muffin tin and let cool 10 minutes (If you don/t have a second muffin tin, use your hands to help mold the cheese around the inverted tin)',
+      'In a large skillet over medium heat, heat oil; Stir in minced garlic, then add ground beef, breaking up meat with a wooden spoon; Cook until beef is no longer pink, about 6 minutes, then drain fat',
+      'Return meat to skillet and season with spices to taste',
+      'Transfer cheese cups to a serving platter; Fill with cooked ground beef, and top with sour cream and chopped tomato.'
+    ],
+  }
+]
--- models/recipe.js
+++ models/recipe.js
@@ -2,11 +2,14 @@ import  mongoose  from 'mongoose'
 import mongooseUniqueValidator from 'mongoose-unique-validator'
 
 const recipeSchema = new mongoose.Schema({
+  course: { type: String, required: true },
   title: { type: String, required: true, unique: true },
   image: { type: String, required: true },
   prepTime: { type: Number, required: true },
   calories: { type: Number, required: true },
-  macros: { type: String, required: true },
+  protein: { type: Number, required: true },
+  carbs: { type: Number, required: true },
+  fats: { type: Number, required: true },
   ingredients: { type: String, required: true },
   preparation: { type: String, required: true },
 })
