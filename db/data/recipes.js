27eb0ad766cd03d8ade595ad239b0f9ed74ff52f
Sally Sghair <sallysghair1@gmail.com>
2022-01-05 17:25:45 +0000

    breakfastadded

--- db/data/recipes.js
+++ db/data/recipes.js
@@ -24,6 +24,224 @@ export default [
     ],
   },
   {
+    course: 'Breakfast',
+    title: 'Granola with berries and almond milk',
+    image: 'https://cdn.app.eative.com/5ebabc8ef3469066263561.jpeg',
+    prepTime: 5,
+    calories: 323,
+    protein: 5,
+    carbs: 14,
+    fats: 27,
+    ingredients: [
+      '1/4 teaspoon cinnamon powder',
+      'vanilla extract',
+      '1/2 cup almond milk',
+      '4 tablespoons heavy cream',
+      '3 tablespoons keto granola',
+      '0 raspberries'
+    ],
+    preparation: [
+      'Put the berries, granola bites and heavy cream into a medium bowl',
+      'Pour the almond milk, add a pinch of cinnamon and/or a few drops of vanilla extract, and enjoy!'
+    ],
+  },
+  {
+    course: 'Breakfast',
+    title: 'Poached egg with ham and veggies',
+    image: 'https://cdn.app.eative.com/5ec5425e1b2cc254374556.jpeg',
+    prepTime: 15,
+    calories: 310,
+    protein: 20,
+    carbs: 7,
+    fats: 22,
+    ingredients: [
+      '1 egg',
+      '2 teaspoons butter',
+      '4-5 cherry tomatoes',
+      '1/2 cup Portobello mushrooms',
+      '2 slices deli turkey meat or turkey ham',
+      '2 small green onions'
+    ],
+    preparation: [
+      'Cut the tomatoes and mushrooms into thin slices',
+      'Melt butter over medium heat in a cooking pan; then add mushrooms and chopped spring onions, and sauté while warming the ham slightly in the same pan',
+      'On an individual plate, build the dish in this order – tomato, mushrooms, onions, ham',
+      'Bring a large pot of water to a boil, then reduce to low heat (or turn off the heat completely); Crack the egg and transfer it to a small bowl',
+      'Add one tablespoon of light-colored vinegar to the pot and stir to create a vortex; Pour the egg into the middle of the vortex and set a timer for 3 minutes',
+      'Once the egg is done, use a slotted spoon to remove the poached egg; Dab with a paper towel to remove excess water',
+      'Place the egg on top of your vegetable pyramid, and enjoy!'
+    ],
+  },
+  {
+    course: 'Breakfast',
+    title: 'Keto blueberry muffins',
+    image: 'https://cdn.app.eative.com/5edf7339bab7e518684317.jpeg',
+    prepTime: 30,
+    calories: 290,
+    protein: 14,
+    carbs: 11,
+    fats: 23,
+    ingredients: [
+      '4 eggs',
+      '1 teaspoon vanilla extract',
+      '1 cup blueberries',
+      '3 cups almond flour',
+      '1/2 cup greek yogurt',
+      '2/3 cup keto-friendly sweetener',
+      '1 tablespoon baking powder'
+    ],
+    preparation: [
+      'Preheat oven to 350°F / 175ºC; Line a 12-cup muffin tin with paper baking cups',
+      'Whisk the eggs in a bowl, then add yogurt, vanilla, and sweetener of your choice; Mix until combined',
+      'Add almond flour and baking powder, and mix well; Add the blueberries, and carefully stir them in',
+      'Divide the batter between 12 muffin cups',
+      'Put the muffin tin in the oven and bake for 25 minutes, or until the muffins are golden brown and firm to touch.'
+    ],
+  },
+  {
+    course: 'Breakfast',
+    title: 'Zucchini oatmeal breakfast bars',
+    image: 'https://cdn.app.eative.com/61434438d085f652328790.png',
+    prepTime: 40,
+    calories: 306,
+    protein: 13,
+    carbs: 10,
+    fats: 26,
+    ingredients: [
+      '2 teaspoons cinnamon powder',
+      '1 tablespoon chia seeds',
+      '1 cup (170g) grated zucchinis',
+      '1/4 cup (65g) peanut butter',
+      '1/2 cup (80g) hulled hemp seeds',
+      '2 tablespoons granulated erythritol'
+    ],
+    preparation: [
+      'Preheat your oven to 375°F (190°C) and line a standard loaf pan with parchment paper',
+      'Stir together finely grated zucchini, peanut butter and sweetener in a bowl until thoroughly combined',
+      'Add in the seeds, cinnamon and a pinch of salt, and stir until completely incorporated; Let the mixture sit for about 5 minutes so the chia seeds have time to form a bit of a gel - The batter should thicken a bit, but still resemble a standard cake batter',
+      'Pour the batter into the pan and spread it around evenly',
+      'Bake for around 35 minutes, until the bars are golden brown and firm to the touch',
+      'Let cool for about 30 minutes before removing from the pan, slicing and enjoying.'
+    ],
+  },
+  {
+    course: 'Breakfast',
+    title: 'Classic tofu scramble',
+    image: 'https://cdn.app.eative.com/5f7dd474ecf13290809100.jpg',
+    prepTime: 25,
+    calories: 304,
+    protein: 9,
+    carbs: 5,
+    fats: 27,
+    ingredients: [
+      '2 tablespoons virgin olive oil',
+      '1/2 teaspoon dried thyme',
+      '1/4 teaspoon paprika powder',
+      '1/2 teaspoon ground cumin',
+      '1/4 teaspoon turmeric powder',
+      '1 garlic cloves',
+      '7oz / 200g firm tofu'
+    ],
+    preparation: [
+      'In a small bowl, combine cumin, thyme, paprika and turmeric; Add 2 tablespoons of water and whisk to combine',
+      'In a large skillet over medium heat, heat oil',
+      'Add tofu (previously drained for 30 minutes and torn into bite-sized pieces) to skillet in an even layer, season with salt and pepper; Let cook undisturbed for 7 to 8 minutes, until undersides are golden',
+      'Add minced garlic to skillet and stir into tofu; Cook until fragrant, 1 to 2 minutes, then add spice mixture',
+      'Stir to incorporate all ingredients, then continue cook, stirring occasionally, for 2 to 3 more minutes, until all water is evaporated and spices are fragrant',
+      'Season to taste with salt and pepper, and serve immediately.'
+    ],
+  },
+  {
+    course: 'Breakfast',
+    title: 'Salmon-filled avocado',
+    image: 'https://cdn.app.eative.com/5eff08f281c1f086970465.jpeg',
+    prepTime: 5,
+    calories: 300,
+    protein: 12,
+    carbs: 6,
+    fats: 25,
+    ingredients: [
+      '1.4oz / 40g smoked salmon',
+      '1/2 small avocados',
+      '1/2 lemons',
+      '1 tablespoon mayonnaise'
+    ],
+    preparation: [
+      'Cut avocado in half and remove the pit',
+      'Place a dollop of mayonnaise or cream cheese in the hollow of the avocado and add smoked salmon on top',
+      'Season to taste with salt and a squeeze of lemon juice for extra flavor (and to keep the avocado from turning brown).'
+    ],
+  },
+  {
+    course: 'Breakfast',
+    title: 'Chia pudding with berries',
+    image: 'https://cdn.app.eative.com/5fb54f93e822e794119194.jpg',
+    prepTime: 5,
+    calories: 305,
+    protein: 5,
+    carbs: 6,
+    fats: 25,
+    ingredients: [
+      '1.4oz / 40g smoked salmon',
+      '1/2 small avocados',
+      '1/2 lemons',
+      '1 tablespoon mayonnaise'
+    ],
+    preparation: [
+      'Cut avocado in half and remove the pit',
+      'Place a dollop of mayonnaise or cream cheese in the hollow of the avocado and add smoked salmon on top',
+      'Season to taste with salt and a squeeze of lemon juice for extra flavor (and to keep the avocado from turning brown).'
+    ],
+  },
+  {
+    course: 'Breakfast',
+    title: 'Cloud Eggs',
+    image: 'https://cdn.app.eative.com/5fb66082b4c9a179067508.jpeg',
+    prepTime: 20,
+    calories: 306,
+    protein: 28,
+    carbs: 2,
+    fats: 19,
+    ingredients: [
+      '2 eggs',
+      '2 tablespoons shredded parmesan cheese',
+      '1 slice deli turkey meat or turkey ham'
+    ],
+    preparation: [
+      'Preheat oven to 450°F / 230ºC and grease a large baking sheet with cooking spray',
+      'Separate egg whites and yolks, placing egg whites in a large bowl and yolks in a small bowl',
+      'Using a whisk or hand mixer, beat egg whites until stiff peaks form; Gently fold in Parmesan and chopped turkey, and season with salt and pepper',
+      'Spoon 2 mounds of egg whites onto prepared baking sheet and indent centers to form nests; Bake until lightly golden, about 3 minutes',
+      'Carefully spoon an egg yolk into center of each nest and season with salt and pepper; Bake until yolks are just set, about 3 minutes more',
+      'Garnish with chopped green onion (optional) before serving.'
+    ],
+  },
+  {
+    course: 'Breakfast',
+    title: 'Ham & Cheese breakfast roll-ups',
+    image: 'https://cdn.app.eative.com/5ee0fb90739e0503726301.png',
+    prepTime: 20,
+    calories: 333,
+    protein: 26,
+    carbs: 4,
+    fats: 22,
+    ingredients: [
+      '2 eggs',
+      '2 tablespoons cheddar cheese',
+      '2 slices deli turkey meat or turkey ham',
+      '1/4 cup whole milk',
+      '1 small green onions'
+    ],
+    preparation: [
+      'Preheat oven to 450°F / 230ºC and grease a large baking sheet with cooking spray',
+      'Separate egg whites and yolks, placing egg whites in a large bowl and yolks in a small bowl',
+      'Using a whisk or hand mixer, beat egg whites until stiff peaks form; Gently fold in Parmesan and chopped turkey, and season with salt and pepper',
+      'Spoon 2 mounds of egg whites onto prepared baking sheet and indent centers to form nests; Bake until lightly golden, about 3 minutes',
+      'Carefully spoon an egg yolk into center of each nest and season with salt and pepper; Bake until yolks are just set, about 3 minutes more',
+      'Garnish with chopped green onion (optional) before serving.'
+    ],
+  },
+  {
     course: 'Lunch',
     title: 'Keto taco cups',
     image: 'https://cdn.app.eative.com/5ee7888028163798738214.jpeg',
@@ -105,7 +323,7 @@ export default [
     ],
   },
   {
-    course: 'Snacks',
+    course: 'Snack',
     title: 'Tomato and cucumber bites with avocado',
     image: 'https://cdn.app.eative.com/5f0f07dd55d83101275236.jpeg',
     prepTime: 7,
@@ -115,10 +333,10 @@ export default [
     fats: 25,
     ingredients: [
       '1 tablespoon virgin olive oil',
-      '1/2 small cucumber/s',
-      '1/2 tomato/es',
-      '1 small celery rib/s',
-      '1/2 small avocado/s'
+      '1/2 small cucumbers',
+      '1/2 tomatoes',
+      '1 small celery ribs',
+      '1/2 small avocados'
     ],
     preparation: [
       'Place chopped cucumber, tomato, avocado and celery into a bowl',
@@ -126,7 +344,7 @@ export default [
     ],
   },
   {
-    course: 'Snacks',
+    course: 'Snack',
     title: 'Crunchy cheese “crackers”',
     image: 'https://cdn.app.eative.com/5ed51886dba8f545451663.jpg',
     prepTime: 7,
