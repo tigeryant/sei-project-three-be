5cc3e5988cd8de267e97a5e1719a997d67eaae20
John Davies <jd.other@hotmail.com>
2022-01-11 13:24:48 +0000

    changed profile image

--- db/seeds.js
+++ db/seeds.js
@@ -18,7 +18,7 @@ async function seed() {
       username: 'admin',
       password: 'pass',
       passwordConfirmation: 'pass',
-      profileImage: 'https://res.cloudinary.com/dlqztlbrv/image/upload/v1641490303/ketokitchen-sei-project-3/tvciqususmfbud2sh1ex.jpg',
+      profileImage: 'https://res.cloudinary.com/dlqztlbrv/image/upload/v1641907448/ketokitchen-sei-project-3/35-350426_profile-icon-png-default-profile-picture-png-transparent_mhkjup.png',
       isAdmin: true,
     })
 
